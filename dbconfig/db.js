de4595e385911eba735a46b5e328ffa58abe1278
Nicholas Siu <ncyk.siu@gmail.com>
2020-11-15 22:29:23 -0800

    expose endpoints for query

---
+++ dbconfig/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const config = require("./config");
+
+const db = mysql.createConnection({
+  host: config.HOST,
+  user: config.USER,
+  password: config.PASSWORD,
+  database: config.DB,
+  port: 3306,
+  insecureAuth: true
+})
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySQL connected");
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-const config = require("./dbconfig/config");
 const express = require("express");
-const mysql = require("mysql");
-
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-const db = mysql.createConnection({
-  host: config.HOST,
-  user: config.USER,
-  password: config.PASSWORD,
-  database: config.DB,
-  port: 3306,
-  insecureAuth: true
-})
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("MySQL connected");
-});
+// Routes
+app.use("/api/athlete", require("./routes/api/athletes"));
 
 const PORT = process.env.PORT || 5000;
 
