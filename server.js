6b6d8d55d6282d7b913a04dc1ffed7b2c33b2f16
Nicholas Siu <ncyk.siu@gmail.com>
2020-11-15 18:40:27 -0800

    db conn err

--- server.js
+++ server.js
@@ -10,16 +10,29 @@ const db = mysql.createConnection({
   host: config.HOST,
   user: config.USER,
   password: config.PASSWORD,
-  database: config.DB
+  database: config.DB,
+  port: 3306
 })
 
 db.connect((err) => {
+  console.log("hit here")
   if (err) {
     throw err;
   }
   console.log("MySQL connected");
 });
 
-const PORT = process.env.PORT || 3306;
+// app.get("/createdb", (req, res) => {
+//   let sql = `CREATE DATABASE ${config.DB}`;
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send("Database created...")
+//   })
+// })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
