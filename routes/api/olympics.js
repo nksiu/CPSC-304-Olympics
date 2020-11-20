c281454c0a11d831a3971ec4bf83d19ac01d1ff3
Barry <bbsin999@hotmail.com>
2020-11-19 23:44:57 -0800

    add comments

--- routes/api/olympics.js
+++ routes/api/olympics.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
     res.json(result);
   })
 })
-
+// Resets DB
 router.get("/reset", (req, res) => {
   const reset = fs.readFileSync(path.join(__dirname, '../reset.sql'), 'utf8').toString();
   sql.query(reset, (err, result) => {
@@ -21,7 +21,7 @@ router.get("/reset", (req, res) => {
     res.json(result);
   })
 })
-
+// Get all from Olympics
 router.get("/getOlympics", (req, res) => {
   sql.query("SELECT * FROM olympics", (err, result) => {
     if (err) throw err;
@@ -29,7 +29,7 @@ router.get("/getOlympics", (req, res) => {
     res.json(result);
   })
 })
-
+// Get all from Country
 router.get("/getCountry", (req, res) => {
   sql.query("SELECT * FROM country", (err, result) => {
     if (err) throw err;
@@ -37,7 +37,7 @@ router.get("/getCountry", (req, res) => {
     res.json(result);
   })
 })
-
+// Insert a sport (Country Name, Medals)
 router.post('/insertSport', (req,res) => {
   var query = `INSERT INTO country(CountryName, NumMedals) VALUES('${req.body.country}', '${req.body.medalsWon}')`
   sql.query(query, (err, result) => {
@@ -46,7 +46,7 @@ router.post('/insertSport', (req,res) => {
     res.json(result);
   })
 })
-
+// Get all from Sport table
 router.get("/getSport", (req, res) => {
   sql.query("SELECT * FROM sport", (err, result) => {
     if (err) throw err;
@@ -54,7 +54,7 @@ router.get("/getSport", (req, res) => {
     res.json(result);
   })
 })
-
+// Get all from plays table
 router.get("/getPlays", (req, res) => {
   sql.query("SELECT * FROM plays", (err, result) => {
     if (err) throw err;
@@ -62,7 +62,7 @@ router.get("/getPlays", (req, res) => {
     res.json(result);
   })
 })
-
+// Delete a specified sport
 router.delete('/deleteSport', (req,res) => {
     var query = `DELETE FROM sport WHERE SportName = '${req.body.sportName}';`
     sql.query(query, (err, result) => {
@@ -71,7 +71,7 @@ router.delete('/deleteSport', (req,res) => {
       res.json(result);
     })
 })
-
+// Update a specified country with a number of medals won
 router.put('/updateCountry', (req,res) => {
     var query = `UPDATE country SET NumMedals = '${req.body.medalsWon}' WHERE CountryName = '${req.body.country}'`
     sql.query(query, (err, result) => {
@@ -80,16 +80,6 @@ router.put('/updateCountry', (req,res) => {
       res.json(result);
     })
 })
-
-// router.get('/project', (req, res)  => {
-//   //TODO replace example with input
-//   var query = "UPDATE country SET NumMedals = '10000000' WHERE CountryName = 'example'"
-//   sql.query(query, (err, result) => {
-//     if (err) throw err;
-//     res.send('Succesfully updated Country');
-//   })
-// })
-
 // Count the number of sports that each Athlete plays
 router.get('/groupBy', (req, res)  => {
   var query = `SELECT Name, COUNT(SportName) 
@@ -102,7 +92,7 @@ router.get('/groupBy', (req, res)  => {
   })
 })
 
-// -- Find the sponsors that sponsor more than $2000 for every sponsorship
+// Find the sponsors that sponsor more than $2000 for every sponsorship
 router.get('/having', (req, res)  => {
   var query = `SELECT sponsorName, SUM(amountSponsored) as totalSponsored
   FROM sponsoredby 
@@ -113,7 +103,7 @@ router.get('/having', (req, res)  => {
     res.json(result);
   })
 })
-
+// get all from hosts_event table
 router.get("/getHostEvent", (req, res) => {
   sql.query("SELECT * FROM hosts_event", (err, result) => {
     if (err) throw err;
@@ -122,7 +112,7 @@ router.get("/getHostEvent", (req, res) => {
   })
 })
 
-// -- For each olympic games with more than 2 events,  find the number of <Type> events
+// For each olympic games with more than 2 events, find the number of <Type> events
 router.get('/nestedAggregation', (req, res)  => {
   var query = `SELECT Year, Count(e.Type) as numEvents
   FROM hosts_event e
@@ -136,16 +126,16 @@ router.get('/nestedAggregation', (req, res)  => {
     res.json(result);
   })
 })
-
+// Get the AthleteName, SponsorName and amountsponsored for each athlete with a sponsor
 router.get("/sponsoredBy", (req, res) => {
-  sql.query("SELECT Name, SponsorName, amountSponsored FROM sponsoredby sb, athlete a WHERE sb.AthleteID = a.AthleteID", (err, result) => {
+  sql.query("SELECT Name as AthleteName, SponsorName, amountSponsored FROM sponsoredby sb, athlete a WHERE sb.AthleteID = a.AthleteID", (err, result) => {
     if (err) throw err;
     console.log(result);
     res.json(result);
   })
 })
 
-// -- Find the athlete (Names) that receive more than <Amount> from ALL of their sponsor
+// Find the athlete (Names) that receive more than <Amount> from ALL of their sponsor
 router.get('/division', (req, res)  => {
   var query = `SELECT DISTINCT a.Name 
   FROM athlete a, sponsoredby sb
@@ -162,7 +152,7 @@ router.get('/division', (req, res)  => {
     res.json(result);
   })
 })
-
+// Choose the AthleteID and Name and the SportName, DateTime, EventName, and Year of the event they're competing in.
 router.get('/projection', (req, res) => {
   var query = `SELECT a.AthleteID, Name, SportName, DateTime, EventName, Year FROM competes_plays cp, athlete a WHERE cp.AthleteID = a.AthleteID`;
   sql.query(query, (err, result) => {
@@ -170,7 +160,7 @@ router.get('/projection', (req, res) => {
     res.json(result);
   })
 })
-
+// Get all from Athletes
 router.get("/getAthlete", (req, res) => {
   sql.query("SELECT * FROM athlete", (err, result) => {
     if (err) throw err;
@@ -178,7 +168,7 @@ router.get("/getAthlete", (req, res) => {
     res.json(result);
   })
 })
-
+// Get all Athletes that received more than amount specified from ANY sponsor
 router.get('/join', (req, res) => {
   var query = `SELECT DISTINCT Name as AthleteName FROM sponsoredby sb, athlete a WHERE sb.AthleteID = a.AthleteID AND amountSponsored > '${req.query.amount}'`;
   sql.query(query, (err, result) => {
